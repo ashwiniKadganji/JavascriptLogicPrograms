1df9c4288986ed8bdadcbea6c3dda957ecb05627
ashwini.k <ashwinikadganji@gmail.com>
2024-04-05 09:02:48 +0530

    string

---
+++ String/string.js
@@ -0,0 +1,9 @@
+// String basic assignments using String Template, File → 03_stringBasicAssig01.js
+// ================================================================
+
+// Please have the function with name → stringBasics( ), No arguments and no return is required
+// 1. ‘My dream company is “your dream company name” ’ log on console
+// 2. Create 3 variables for My Hobbies are: , “Your hobby1”, “Your hobby2”, “Your hobby3”
+// 2.1. Log all hobbies on console on same line
+// 2.2. Sum the total number of characters that is available in hobby1, hobby2 and
+// hobby3 and log on console
---
+++ String/stringHandsOn.js
@@ -0,0 +1,16 @@
+// Use string template to log output on console
+// Note: Log the result on console with meaningful message like - “Length of string is: 45”
+// ==========================================================================
+// Given a String : “ Hey you are doing good, keep it up ”;
+// Steps: please create a function ‘stringHandsOn’ with no arguments, no return value, pls don’t forget to
+// invoke this function
+// 1. Print the string as it is on console
+// 2. Calculate length of the String
+// 3. Remove the leading and trailing extra spaces and log string on console with it’s length
+// 4. Print the total number extra spaces count that is removed in step 3.
+// 5. Print the first and last character on the same line after trim()
+// 6. Print the count of total words available in the string after step 3
+// 7. Print the index of word “good” from the given string
+// 8. Print the substring starting from index 22, using substring() and slice()
+// 9. Check, is string ends with word “up” after step 3?
+// 10. Check is string starts with word “Hey” after trimming → i.e after step 3 output string value
